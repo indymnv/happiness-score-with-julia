fa0577ce8b841c2cc13d1f1ef014925312c5b5f5
indymnv <indy.mnv@gmail.com>
2022-11-18 20:08:00 +0900

    preparing the map

--- src/train.jl
+++ src/train.jl
@@ -13,8 +13,8 @@ using PyCall
 df = DataFrame(CSV.File("./data/2021.csv",normalizenames =true),)
 
 float_df = select(df, findall(col -> eltype(col) <: Float64, eachcol(df)))
-
 float_df = float_df[:,Not(names(select(float_df, r"Explained")))]
+
 
 function normalize_features(df)
     for col in names(df)
@@ -67,3 +67,30 @@ df.cluster = cluster.labels_
 df.cluster
 
 filter(row ->row.cluster ==0,df).Country_name
+
+import GeoMakie, CairoMakie
+
+# First, make a surface plot
+lons = -180:180
+lats = -90:90
+field = [exp(cosd(l)) + 3(y/90) for l in lons, y in lats]
+
+fig = CairoMakie.Figure()
+ax = GeoAxis(fig[1,1])
+sf = GeoMakie.surface!(ax, lons, lats, field; shading = false)
+cb1 = Colorbar(fig[1,2], sf; label = "field", height = Relative(0.65))
+
+using GeoMakie.GeoJSON
+countries_file = download("https://datahub.io/core/geo-countries/r/countries.geojson")
+countries = GeoJSON.read(read(countries_file, String))
+
+df_world = DataFrame(countries)
+
+
+n = length(countries)
+hm = GeoMakie.poly!(ax, countries; color= 1:n, colormap = :dense,
+    strokecolor = :black, strokewidth = 0.5,
+)
+GeoMakie.translate!(hm, 0, 0, 100) # move above surface plot
+
+save("../01-happines/img/map.png", fig)
