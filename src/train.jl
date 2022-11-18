2cd3e3e7fc9519d98da2f5daa2a0d0a68c3cabe9
indymnv <indy.mnv@gmail.com>
2022-11-19 08:26:29 +0900

    encapsule in functions part 1

--- src/train.jl
+++ src/train.jl
@@ -8,33 +8,23 @@ using Random
 using ScikitLearn
 using PyCall
 Plots.theme(:ggplot2)
-
 @sk_import preprocessing: StandardScaler
 @sk_import cluster: KMeans
 
-df = DataFrame(CSV.File("./data/2021.csv",normalizenames =true),)
+function read_file()
+    df = DataFrame(CSV.File("./data/2021.csv",normalizenames =true),)
 
-float_df = select(df, findall(col -> eltype(col) <: Float64, eachcol(df)))
-float_df = float_df[:,Not(names(select(float_df, r"Explained")))]
-
-
-function normalize_features(df)
-    for col in names(df)
-        df[!,col] = (df[!,col].-mean(df[!,col]))./std(df[!,col])
-    end
-    return df
+    float_df = select(df, findall(col -> eltype(col) <: Float64, eachcol(df)))
+    float_df = float_df[:,Not(names(select(float_df, r"Explained")))]   
+    select!(float_df, Not([:Standard_error_of_ladder_score, 
+                           :Ladder_score_in_Dystopia, 
+                           :Dystopia_residual,:upperwhisker,
+                           :lowerwhisker]))
+    return float_df, df
 end
 
-
-#X = normalize_features(float_df)
-select!(float_df, Not([:Standard_error_of_ladder_score, :Ladder_score_in_Dystopia, :Dystopia_residual,:upperwhisker, :lowerwhisker]))
-
-
+float_df, df = read_file()
 X = fit_transform!(StandardScaler(), Matrix(float_df))
-
-names(float_df)
-
-size(input)
 
 wcss = []
 
@@ -49,7 +39,9 @@ for n in 1:10
     push!(wcss, cluster.inertia_)
 end
 
-plot(wcss)
+plot(wcss, title = "wcss in each cluster",
+    xaxis = "cluster",
+   yaxis = "Wcss")
 
 Random.seed!(123)
 cluster =KMeans(n_clusters=3)
@@ -72,6 +64,13 @@ df.cluster = cluster.labels_
 df.cluster
 
 filter(row ->row.cluster ==0,df).Country_name
+
+histogram(filter(row ->row.cluster ==0,df).Ladder_score)
+histogram!(filter(row ->row.cluster ==1,df).Ladder_score)
+histogram!(filter(row ->row.cluster ==2,df).Ladder_score)
+
+
+filter(row ->row.Country_name =="Chile",df).cluster
 
 import GeoMakie, CairoMakie
 
